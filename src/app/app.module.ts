dfdf13ed619316aef469849a28eebaa5374aeb19
Haosik <noobbaster@gmail.com>
2017-07-27 23:43:36 +0300

    watch 30% ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClockControlComponent } from './clock-control/clock-control.component';
+import { ClockWatchComponent } from './clock-watch/clock-watch.component';
+import { ClockService } from './clock.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClockControlComponent,
+    ClockWatchComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
