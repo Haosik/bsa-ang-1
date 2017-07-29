d065e7fa48679cdf049660e0aaa1490b5c8da418
Haosik <noobbaster@gmail.com>
2017-07-29 19:54:28 +0300

    countdown done

--- src/app/clock-countdown/clock-countdown.component.ts
+++ src/app/clock-countdown/clock-countdown.component.ts
@@ -9,8 +9,8 @@ import { WatchService } from '../watch.service';
 export class ClockCountdownComponent implements OnInit {
 
   constructor(private watchService: WatchService) { }
-  
-   ngOnInit() {  }
+
+  ngOnInit() { }
 
   public lapTime = 0;
   public timer = 0;
@@ -27,20 +27,25 @@ export class ClockCountdownComponent implements OnInit {
   private interval: any;
   public laps: any[] = [];
 
+  setSpentTime() {
+    this.hoursSpent = this.watchService.pretty('hours', this.timer);
+    this.minutesSpent = this.watchService.pretty('minutes', this.timer);
+    this.secondsSpent = this.watchService.pretty('seconds', this.timer);
+    this.milisecondsSpent = this.watchService.pretty('miliseconds', this.timer);
+  }
+  setCurrLapTime() {
+    this.currLaphours = this.watchService.pretty('hours', this.lapTime);
+    this.currLapMinutes = this.watchService.pretty('minutes', this.lapTime);
+    this.currLapSec = this.watchService.pretty('seconds', this.lapTime);
+    this.currLapMilisec = this.watchService.pretty('miliseconds', this.lapTime);
+  }
   start() {
     this.active = true;
     this.interval = setInterval(() => {
       this.timer += 100;
       this.lapTime += 100;
-      this.hoursSpent = this.watchService.pretty('hours', this.timer);
-      this.minutesSpent = this.watchService.pretty('minutes', this.timer);
-      this.secondsSpent = this.watchService.pretty('seconds', this.timer);
-      this.milisecondsSpent = this.watchService.pretty('miliseconds', this.timer);
-
-      this.currLaphours = this.watchService.pretty('hours', this.lapTime);
-      this.currLapMinutes = this.watchService.pretty('minutes', this.lapTime);
-      this.currLapSec = this.watchService.pretty('seconds', this.lapTime);
-      this.currLapMilisec = this.watchService.pretty('miliseconds', this.lapTime);
+      this.setSpentTime();
+      this.setCurrLapTime();
     }, 100);
   }
 
@@ -53,21 +58,42 @@ export class ClockCountdownComponent implements OnInit {
     this.pause();
     this.timer = 0;
     this.lapTime = 0;
+    clearInterval(this.interval);
+    this.setSpentTime();
+    this.setCurrLapTime();
+    this.laps = [];
   }
 
   prevLapTime: number = 0;
-  currLapTime: number = 0;
   avgLapTime: number = 0;
+  slowerLap: boolean = false;
+
+  setAvgLapTime() {
+    let laps = this.laps;
+    let totalTime = 0;
+    laps.forEach((el, ind) => {
+      totalTime += el.time;
+    });
+    this.avgLapTime = Math.round(totalTime / laps.length);
+  }
 
   lap() {
+    if (this.prevLapTime > this.lapTime || this.laps.length < 1) {
+      this.slowerLap = false;
+    } else {
+      this.slowerLap = true;
+    };
     this.laps.push({
       time: this.lapTime,
       hours: this.currLaphours,
       minutes: this.currLapMinutes,
       seconds: this.currLapSec,
       miliseconds: this.currLapMilisec,
+      slowerLap: this.slowerLap
     });
-
+    this.setAvgLapTime();
+    
+    this.prevLapTime = this.lapTime;
     this.lapTime = 0;
   }
 
